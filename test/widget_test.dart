01a69892e6b6af3f3dc12e922df4e5660bf191e1
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-04-25 09:51:38 +0100

    Text(

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:packt_advanced_flutter/main.dart';
+import 'file:///D:/Users/DEV/AndroidStudioProjects/packt_advanced_flutter/lib/flutter_advanced_1_5/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
